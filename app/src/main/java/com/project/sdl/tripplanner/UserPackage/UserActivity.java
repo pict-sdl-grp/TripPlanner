7529e0fa2b82cbd249c3c2857e0c38585a8ddd25
Manish Chougule <manish@rxlab.solutions>
2019-08-03 19:01:06 +0530

    initial commit UserPackage created

---
+++ app/src/main/java/com/project/sdl/tripplanner/UserPackage/UserActivity.java
@@ -0,0 +1,15 @@
+package com.project.sdl.tripplanner.UserPackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.sdl.tripplanner.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}

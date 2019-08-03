e72d19c9bc54d2d46c54e9e07c7725c41f450723
Manish Chougule <manish@rxlab.solutions>
2019-08-04 04:30:42 +0530

    login/signup ui done

--- app/src/main/java/com/project/sdl/tripplanner/AuthPackage/AuthActivity.java
+++ app/src/main/java/com/project/sdl/tripplanner/AuthPackage/AuthActivity.java
@@ -1,18 +1,56 @@
 package com.project.sdl.tripplanner.AuthPackage;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.IgnoreExtraProperties;
 import com.project.sdl.tripplanner.R;
-
-import java.util.UUID;
 
 
 public class AuthActivity extends AppCompatActivity {
+
+    TextView switchText1;
+    TextView switchText2;
+    TextView appbar_title1;
+    TextView appbar_title2;
+    RelativeLayout login_relative_layout;
+    RelativeLayout signup_relative_layout;
+    ImageView auth_appbar;
+
+    Boolean authMode = true;
+
+    public void onChangeAuthmode(View view){
+
+        authMode = !authMode;
+
+        if(authMode) {
+            login_relative_layout.setVisibility(View.VISIBLE);
+            signup_relative_layout.setVisibility(View.INVISIBLE);
+
+            getSupportActionBar().setTitle("Login");
+            appbar_title1.setText("Hello again, login to");
+            appbar_title2.setText("explore your place!");
+            switchText1.setText("Didn't have account yet?");
+            switchText2.setText("SignUp");
+            switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        }else{
+            login_relative_layout.setVisibility(View.INVISIBLE);
+            signup_relative_layout.setVisibility(View.VISIBLE);
+
+            getSupportActionBar().setTitle("Signup");
+            appbar_title1.setText("Enjoy everything with our");
+            appbar_title2.setText("wonderful trip advice!");
+            switchText1.setText("Didn't have account yet?");
+            switchText2.setText("SignUp");
+            switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        }
+
+    }
 
 
     @Override
@@ -20,8 +58,22 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        //hide action bar
-        getSupportActionBar().hide();
+        login_relative_layout = (RelativeLayout)findViewById(R.id.login_relative_layout);
+        signup_relative_layout = (RelativeLayout)findViewById(R.id.signup_relative_layout);
+        auth_appbar = (ImageView)findViewById(R.id.auth_appbar);
+        switchText1 = (TextView)findViewById(R.id.switch_text1);
+        switchText2 = (TextView)findViewById(R.id.switch_text2);
+        appbar_title1 = (TextView)findViewById(R.id.appbar_title1);
+        appbar_title2 = (TextView)findViewById(R.id.appbar_title2);
+
+        //set action bar title
+        getSupportActionBar().setTitle("Login");
+        getSupportActionBar().setElevation(0);
+
+        auth_appbar.setImageResource(R.drawable.auth_appbar);
+
+
+        switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
     }
 

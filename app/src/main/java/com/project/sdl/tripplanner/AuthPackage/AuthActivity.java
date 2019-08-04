47dba66e5f21173c7de64d635d04c956f734ae08
Manish Chougule <manish@rxlab.solutions>
2019-08-04 12:28:49 +0530

    some ui changes

--- app/src/main/java/com/project/sdl/tripplanner/AuthPackage/AuthActivity.java
+++ app/src/main/java/com/project/sdl/tripplanner/AuthPackage/AuthActivity.java
@@ -1,6 +1,7 @@
 package com.project.sdl.tripplanner.AuthPackage;
 
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -23,7 +24,7 @@ public class AuthActivity extends AppCompatActivity {
     ImageView auth_appbar;
     Button auth_button;
 
-    Boolean authMode = true;
+    Boolean authMode = true; //if auth mode is true --- login mode
 
     public void onChangeAuthmode(View view){
 
@@ -34,24 +35,34 @@ public class AuthActivity extends AppCompatActivity {
             signup_relative_layout.setVisibility(View.INVISIBLE);
 
             getSupportActionBar().setTitle("Login");
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
+                    .getColor(R.color.colorPrimary)));
+            auth_appbar.setImageResource(R.drawable.auth_login_appbar);
             appbar_title1.setText("Hello again, login to");
             appbar_title2.setText("explore your place!");
             switchText1.setText("Didn't have account yet?");
             switchText2.setText("Signup");
-            auth_button.setText("login");
+            switchText2.setTextColor(getResources().getColor(R.color.colorPrimary));
             switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            auth_button.setText("login");
+            auth_button.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.colorPrimary));
+
         }else{
             login_relative_layout.setVisibility(View.INVISIBLE);
             signup_relative_layout.setVisibility(View.VISIBLE);
 
             getSupportActionBar().setTitle("Signup");
+            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
+                    .getColor(R.color.colorSecondary)));
+            auth_appbar.setImageResource(R.drawable.auth_signup_appbar);
             appbar_title1.setText("Enjoy everything with our");
             appbar_title2.setText("wonderful trip advice!");
             switchText1.setText("Do you already have an account?");
             switchText2.setText("Login");
-            auth_button.setText("signup");
+            switchText2.setTextColor(getResources().getColor(R.color.colorSecondary));
             switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-
+            auth_button.setText("signup");
+            auth_button.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.colorSecondary));
         }
 
     }
@@ -71,12 +82,12 @@ public class AuthActivity extends AppCompatActivity {
         appbar_title2 = (TextView)findViewById(R.id.appbar_title2);
         auth_button = (Button)findViewById(R.id.auth_button);
 
-        //set action bar title
+        //set action bar
         getSupportActionBar().setTitle("Login");
         getSupportActionBar().setElevation(0);
-
-        auth_appbar.setImageResource(R.drawable.auth_appbar);
-
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
+                .getColor(R.color.colorPrimary)));
+        auth_appbar.setImageResource(R.drawable.auth_login_appbar);
 
         switchText2.setPaintFlags(switchText2.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 

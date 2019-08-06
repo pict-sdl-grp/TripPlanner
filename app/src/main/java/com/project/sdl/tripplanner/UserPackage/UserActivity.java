aa7920ccbd0e9f20ca1cf4b80b672c83ff7f51ab
CS-18 <csonwanecs18@gmail.com>
2019-08-06 19:43:55 +0530

    TabLayout Added at bottom

--- app/src/main/java/com/project/sdl/tripplanner/UserPackage/UserActivity.java
+++ app/src/main/java/com/project/sdl/tripplanner/UserPackage/UserActivity.java
@@ -1,17 +1,20 @@
 package com.project.sdl.tripplanner.UserPackage;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.project.sdl.tripplanner.AuthPackage.AuthActivity;
+import com.project.sdl.tripplanner.ProfilePackage.ProfileActivity;
 import com.project.sdl.tripplanner.R;
 import android.view.View;
 
 public class UserActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    private TabLayout bottomTab;
 
     public void onClickLogout(View view){
         mAuth.signOut();
@@ -25,11 +28,36 @@ public class UserActivity extends AppCompatActivity {
         super.onBackPressed();
 
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
         mAuth = FirebaseAuth.getInstance();
+        bottomTab = findViewById(R.id.bottomTablayout);
+
+        bottomTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()){
+                    case 0:
+                        break;
+
+                    case 3:
+                        Intent profileAcitvity = new Intent(UserActivity.this,ProfileActivity.class);
+                        startActivity(profileAcitvity);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }

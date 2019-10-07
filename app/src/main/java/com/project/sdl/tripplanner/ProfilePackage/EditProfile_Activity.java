325cdd4849197908f110f2ad3a824e973fcc225e
CS-18 <csonwanecs18@gmail.com>
2019-10-07 14:32:32 +0530

    Some CHange

--- app/src/main/java/com/project/sdl/tripplanner/ProfilePackage/EditProfile_Activity.java
+++ app/src/main/java/com/project/sdl/tripplanner/ProfilePackage/EditProfile_Activity.java
@@ -117,6 +117,7 @@ public class EditProfile_Activity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+
             Uri selectedImage = data.getData();
             String[] filePathColumn = { MediaStore.Images.Media.DATA };
             Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
@@ -137,6 +138,8 @@ public class EditProfile_Activity extends AppCompatActivity {
 
             editor.putString(PROFILE,encodedImage);
             editor.commit();
+
+            Log.d("OnResult OF Gallery","PRofile image selected from gallery");
         }
     }
 
